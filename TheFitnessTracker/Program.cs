28781997638434d9db1f0526f99a4ee6cb581e5b
Szaszki Szilárd Zsombor <szaszi228@logiker.hu>
2026-02-19 19:57:25 +0100

    Adatbázis kapcsolat létrehozása, tábla létrehozása ha szükséges

--- TheFitnessTracker/Program.cs
+++ TheFitnessTracker/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySqlConnector;
 namespace TheFitnessTracker
 {
     internal static class Program
@@ -13,8 +14,23 @@ namespace TheFitnessTracker
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+
+            string cs = @"server=localhost;userid=root;port=3306;password=;database=test";
+
+            using (MySqlConnection con = new MySqlConnection(cs))
+            {
+                con.Open();
+                var cmd = new MySqlCommand();
+                cmd.Connection = con;
+
+                cmd.CommandText = @"CREATE TABLE IF NOT EXISTS adatok(id INT AUTO_INCREMENT PRIMARY KEY ,sportag TEXT, datum DateTime, idotartam INT, helyszin TEXT)";
+                cmd.ExecuteNonQuery();
+            }
+
             Application.Run(new Form1());
 
 
